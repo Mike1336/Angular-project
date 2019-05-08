2afc7cf22c9a4a4bfa34f0fdfc00762a75add822
Mikhail <muzhikovm@bk.ru>
2019-05-08 16:41:19 +0300

    add templates for personal pages for item and employee and not found page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,27 @@ import { AppComponent } from './app.component';
 import { ItemsComponent } from './items/items.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeeComponent } from './employee/employee.component';
+import { ItemComponent } from './item/item.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {path: 'inventory', component: ItemsComponent},
   {path: 'staff', component: EmployeesComponent},
   {path: '', redirectTo: 'inventory', pathMatch: 'full'},
-
-  // {path: '**', component: LoginRouteComponent}
+  {path: 'item', component: ItemComponent},
+  {path: 'employee', component: EmployeeComponent},
+  {path: '**', component: NotFoundComponent}
  ];
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemsComponent,
-    EmployeesComponent
+    EmployeesComponent,
+    EmployeeComponent,
+    ItemComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
